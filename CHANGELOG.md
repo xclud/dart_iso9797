7a41bef2fc38ce1839598c948ff7591e82b01bcb
Mahdi <mkfard@outlook.com>
2024-06-19 03:08:25 +0330

    ANSI X9.19 MAC implemented.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,10 @@
+## 0.0.3
+
+- ANSI X9.19 MAC implemented.
+
 ## 0.0.2
 
 - Implements alogrithm 1 and 3 and padding 2.
-
 ## 0.0.1
 
 - Initial version.
--- lib/src/algorithm3.dart
+++ lib/src/algorithm3.dart
@@ -31,3 +31,24 @@ Uint8List algorithm3(
 
   return mac;
 }
+
+/// ISO 9797 MAC Algorithm 3.
+///
+/// [key] Must be 16 bytes.
+Uint8List algorithm3Mac(List<int> key, List<int> message) {
+  if (message.length % 8 != 0) {
+    final copyOfData = message.toList();
+    while (copyOfData.length % 8 != 0) {
+      copyOfData.add(0);
+    }
+
+    message = Uint8List.fromList(copyOfData);
+  }
+
+  final mac = algorithm3(key, message, PaddingMode.method1);
+
+  final macU = mac.take(4).map((e) => e.toRadixString(16)).join().toUpperCase();
+
+  final result = macU.codeUnits.take(8);
+  return Uint8List.fromList(result.toList());
+}
